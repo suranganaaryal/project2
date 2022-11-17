164e6b1a7860dccb2e4f2df3d3ac42ca32745832
suranganaaryal <surangana.aryal@bison.howard.edu>
2022-11-16 21:26:07 -0500

    error message and input storage

--- project2.s
+++ project2.s
@@ -1,17 +1,6 @@
 .data
-
+prompt: .asciiz "Unrecognized input"
 input: .space 1001 
-enter: .asciiz "\n"
-# N = 26 + (X % 11), N(base) = 30
-# M = N - 10, M = 20
+string: .space 5
 .text
 
-main:
-li $v0, 8
-la $a0, input
-li $a1, 11
-syscall
-
-enterInput:
-move $t3, $a0
-lb $s2, ($t3)
